1cc9f44c2b8fe6a75d15e5695a8244628e7ca89f
johnholl <johnholl@umich.edu>
2016-07-17 12:50:18 -0400

    added Breakout DQN

--- Atari/Breakout_DQN.py
+++ Atari/Breakout_DQN.py
@@ -9,155 +9,144 @@ from PIL import Image
 ## the cartpole simulator and outputs 0 or 1 at each time
 ## step, which controls left and right movement of the cart.
 
-class Controller:
-    FUTURE_DISCOUNT = 0.99
-    RANDOM_ACTION_DECAY = 0.99
-    MIN_RANDOM_ACTION_PROBABILITY = 0.1
-    CONV1_SIZE =
-    CONV2_SIZE =
 
-    HIDDEN1_SIZE = 20
-    HIDDEN2_SIZE = 20
-    NUM_EPISODES = 1000000
-    MAX_STEPS = 1000
-    LEARNING_RATE = 0.001
-    MINIBATCH_SIZE = 50
-    REG_FACTOR = 0.001
-    RANDOM_ACTION_PROBABILITY = 0.5
-    WEIGHT_UPDATE_FREQUENCY = 300
-    MEMORY = []
-    MEMORY_CAPACITY = 10000
-    LOG_DIR = '/tmp/Controller'
+def make_network():
+    # Initializes tensorflow graph. Input is 4 84x84 B&W array shaped as [84,84,4]
+    input = tf.placeholder('float', [None, 84, 84, 4])
+    layer1_weights = tf.Variable(tf.truncated_normal([8,8,4,16], stddev=0.01))
+    layer1_bias = tf.Variable(tf.constant(0.1, shape=[16]))
+    conv1 = tf.nn.relu(tf.nn.conv2d(input, layer1_weights, strides=[1,4,4,1], padding='VALID') + layer1_bias)
+    layer2_weights = tf.Variable(tf.truncated_normal([4,4,16,32], stddev=0.01))
+    layer2_bias = tf.Variable(tf.constant(0.1, shape=[32]))
+    conv2 = tf.nn.relu(tf.nn.conv2d(conv1, layer2_weights, strides=[1,2,2,1], padding='VALID') + layer2_bias)
+    conv2_reshape = tf.reshape(conv2, [-1, 2592])
+    layer3_weights = tf.Variable(tf.truncated_normal([2592, 256], stddev=0.01))
+    layer3_bias = tf.Variable(tf.constant(0.1, shape=[256]))
+    layer3 = tf.nn.relu(tf.matmul(conv2_reshape, layer3_weights)+layer3_bias)
+    layer4_weights = tf.Variable(tf.truncated_normal([256, 6], stddev=0.01))
+    layer4_bias = tf.Variable(tf.constant(0.1, shape=[6]))
+    output = tf.matmul(layer3, layer4_weights) + layer4_bias
 
-    def __init__(self):
-        self.env = gym.make('CartPole-v0')
+    weights = [layer1_weights, layer2_weights, layer3_weights, layer4_weights,
+               layer1_bias, layer2_bias, layer3_bias, layer4_bias]
 
-    def make_network(self):
-        # define input, output, weight, and bias variables for network
-        self.input = tf.placeholder("float", [None, 4])
-        self.layer_1_weights = tf.Variable(tf.truncated_normal([4, self.HIDDEN1_SIZE],stddev=0.01))
-        self.layer_1_biases = tf.Variable(tf.constant(0.01, shape=[self.HIDDEN1_SIZE]))
-        self.layer_2_weights = tf.Variable(tf.truncated_normal([self.HIDDEN1_SIZE, self.HIDDEN2_SIZE],stddev=0.01))
-        self.layer_2_biases = tf.Variable(tf.constant(0.01, shape=[self.HIDDEN2_SIZE]))
-        self.output_layer_weights = tf.Variable(tf.truncated_normal([self.HIDDEN2_SIZE, 2],stddev=0.01))
-        self.output_layer_biases = tf.Variable(tf.constant(0.01, shape=[2]))
+    targets = tf.placeholder('float', None)
+    action = tf.placeholder('float', [None,6])
+    action_readout = tf.reduce_sum(tf.mul(output, action), reduction_indices=1)
+    loss = tf.reduce_mean(tf.square(tf.sub(action_readout, targets)))
+    train_operation = tf.train.AdamOptimizer(0.01).minimize(loss)
+
+    return input, action, weights, targets, output, train_operation
 
 
-        # define layers
-        self.hidden_layer_1 = tf.nn.tanh(tf.add(tf.matmul(self.input, self.layer_1_weights), self.layer_1_biases))
-        self.hidden_layer_2 = tf.nn.tanh(tf.add(tf.matmul(self.hidden_layer_1, self.layer_2_weights), self.layer_2_biases))
-        self.output = tf.add(tf.matmul(self.hidden_layer_2, self.output_layer_weights), self.output_layer_biases)
-        self.action = tf.placeholder("float", [None,2])
-        self.targets = tf.placeholder("float", [None])
+def preprocess(obs):
+    img = Image.fromarray(obs, 'RGB')
+    img = img.convert('L')
+    img = img.resize((84,84))
+    new_obs = np.array(img)
+    return new_obs
 
-        # weights and readout will be useful for calculations
-        self.weights = [self.layer_1_weights, self.layer_1_biases,
-                        self.layer_2_weights, self.layer_2_biases,
-                        self.output_layer_weights, self.output_layer_biases]
-        self.readout_action = tf.reduce_sum(tf.mul(self.output, self.action), reduction_indices=1)
+def train(input, action_one_hots, weights, targets, output, train_operation, num_episodes=1000, max_steps=800):
+    # Contains full training procedure. Main loop over num of episodes.
+    # state will consist of previous 4 frames.
+    # actions determined by eps. greedy policy w.r.t current Q-values
+    # state action reward tuples will be collected and placed in a replay memory.
+    # minibatch samples will be drawn from replay memory for training
+    env = gym.make('Breakout-v0')
+    session = tf.Session()
+    session.run(tf.initialize_all_variables())
+    target_weights = session.run(weights)
+    replay_memory = []
+    episode_step_count = []
+    episode_count = 0
+    total_steps = 0
 
-        # define loss and training method
-        self.loss = tf.reduce_mean(tf.square(tf.sub(self.readout_action,self.targets)))
-        for w in [self.layer_1_weights, self.layer_2_weights]:
-            self.loss+=self.REG_FACTOR*tf.reduce_sum(tf.square(w))
-        self.train_operation = tf.train.AdamOptimizer(self.LEARNING_RATE).minimize(self.loss)
+    for episode in range(num_episodes):
+        obs1 = env.reset()
+        obs2, _, _, _ = env.step(env.action_space.sample())
+        obs3, _, _, _ = env.step(env.action_space.sample())
+        obs4, _, _, _ = env.step(env.action_space.sample())
+        obs1, obs2, obs3, obs4 = preprocess(obs1), preprocess(obs2), preprocess(obs3), preprocess(obs4)
+        state = [obs1, obs2, obs3, obs4]
+        state = np.transpose(state, (1, 2, 0))
+        print(np.shape(state))
+        steps = 0
 
-    # train will run a full episode of CartPole and construct a batch from it
-    # upon completion of the episode (or perhaps some # time steps at most), the
-    # network will train on the data
-    def train(self, num_episodes=NUM_EPISODES):
-        self.session=tf.Session()
+        for step in range(max_steps):
+            if random.random() > 0.1:
+                Q_vals = session.run(output, feed_dict={input: [state]})
+                action = Q_vals.argmax()
+            else:
+                action = env.action_space.sample()
 
-        # set up Tensorboard summary
-        tf.scalar_summary('loss', self.loss)
-        self.summary = tf.merge_all_summaries()
-        self.summary_writer = tf.train.SummaryWriter(self.LOG_DIR, self.session.graph)
+            obs1 = obs2
+            obs2 = obs3
+            obs3 = obs4
+            obs4, reward, done, info = env.step(action)
+            obs4 = preprocess(obs4)
+            new_state = [obs1, obs2, obs3, obs4]
+            new_state = np.transpose(new_state, (1, 2, 0))
 
-        self.session.run(tf.initialize_all_variables())
+            if done:
+                reward = -100
 
-        total_steps = 0
-        episode_step_counts = []
-        target_weights = self.session.run(self.weights)
+            replay_memory.append((state, action, reward, new_state, done))
+            state = new_state
 
-        for episode in range(num_episodes):
-            state = self.env.reset()
-            steps = 0
-            for step in range(self.MAX_STEPS):
-                if random.random() > self.RANDOM_ACTION_PROBABILITY:
-                    q_values = self.session.run(self.output, feed_dict={self.input: [state]})
-                    action =q_values.argmax()
-                else:
-                    action = self.env.action_space.sample()
+            if len(replay_memory) > 10000:
+                replay_memory.pop(0)
 
-                self.update_policy_randomness()
-                obs, reward, done, info = self.env.step(action)
+            if len(replay_memory) >= 50:
+                # here is where the training procedure takes place
 
-                if done:
-                    reward = -100
+                # compute the one step q-values w.r.t. old weights (ie y in the loss function (y-Q(s,a,0))^2)
+                # Also defines the one-hot readout action vectors
+                minibatch = random.sample(replay_memory, 50)
+                next_states = [m[3] for m in minibatch]
+                feed_dict = {input: next_states}
+                feed_dict.update(zip(weights, target_weights))
+                q_vals = session.run(output, feed_dict=feed_dict)
+                max_q = q_vals.max(axis=1)
+                target_q = np.zeros(50)
+                action_list = np.zeros((50,6))
+                for i in range(50):
+                    _, action_index, reward, _, terminal = minibatch[i]
+                    target_q[i] = reward
+                    if not terminal:
+                        target_q[i] =+ 0.99*max_q[i]
 
-                self.MEMORY.append((state, action, reward, obs, done))
+                    action_list[i][action_index] = 1.0
 
-                if(len(self.MEMORY) > self.MEMORY_CAPACITY):
-                    self.MEMORY.pop(0)
+                states = [m[0] for m in minibatch]
+                feed_dict = {input: states, targets: target_q, action_one_hots: action_list}
+                session.run(train_operation, feed_dict=feed_dict)
 
-                state = obs
+            total_steps += 1
+            steps += 1
 
-                if(len(self.MEMORY) >= 50):
-                    minibatch = random.sample(self.MEMORY, 50)
-                    next_states = [m[3] for m in minibatch]
-                    feed_dict = {self.input: next_states}
-                    feed_dict.update(zip(self.weights, target_weights))
-                    q_values = self.session.run(self.output, feed_dict=feed_dict)
-                    max_q_values = q_values.max(axis=1)
+            if done:
+                break
 
-                    # compute target q values
-                    target_q_values = np.zeros(self.MINIBATCH_SIZE)
-                    action_list = np.zeros((self.MINIBATCH_SIZE,2))
-                    for i in range(self.MINIBATCH_SIZE):
-                        _, action, reward, _, terminal = minibatch[i]
-                        target_q_values[i] = reward
-                        if not terminal:
-                            target_q_values[i] += self.FUTURE_DISCOUNT*max_q_values[i]
-                        action_list[i][action] = 1.0
+        episode_step_count.append(steps)
+        mean_steps = np.mean(episode_step_count[-100:])
+        print("Training episode = {}, Total steps = {}, Last 100 mean steps = {}"
+              .format(episode, total_steps, mean_steps))
 
-                    states = [m[0] for m in minibatch]
-                    self.session.run(self.train_operation, feed_dict={
-                        self.input: states,
-                        self.targets: target_q_values,
-                        self.action: action_list
-                    })
-
-                total_steps += 1
-                steps += 1
-
-                if done:
-                    break
-
-            episode_step_counts.append(steps)
-            mean_steps = np.mean(episode_step_counts[-100:])
-            print("Training episode = {}, Total steps = {}, Last-100 mean steps = {}"
-                  .format(episode, total_steps, mean_steps))
-
-            if episode % self.WEIGHT_UPDATE_FREQUENCY == 0:
-                target_weights = self.session.run(self.weights)
-
-    def update_policy_randomness(self):
-        self.RANDOM_ACTION_PROBABILITY *= self.RANDOM_ACTION_DECAY
-        if self.RANDOM_ACTION_PROBABILITY < self.MIN_RANDOM_ACTION_PROBABILITY:
-            self.RANDOM_ACTION_PROBABILITY = self.MIN_RANDOM_ACTION_PROBABILITY
-
-    def preprocess_input(self, obs):
-        # this function converts images to a black and white 80 x 80 numpy array
-        img = Image.fromarray(obs)
-        img = img.resize((80,80))
-        img = img.convert('L')
-        obs = np.asarray(img)
-        return obs
+        if episode % 100 == 0:
+            target_weights = session.run(weights)
 
 
 
-controller = Controller()
-controller.make_network()
-controller.train()
+def test_network():
+    # create a new env
+    # play using greedy policy w.r.t. Q values
+    # record episode length
+    return True
+
+def update_randomeness():
+    # can add this in if you want to anneal randomness. Without it we'll keep constant randomness .1
+    return True
 
 
+# i, a, w, t, o, tr = make_network()
+# train(i, a, w, t, o, tr)
