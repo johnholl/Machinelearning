11044e45561945bccfeafb908ef8477f06db9532
johnholl <johnholl@umich.edu>
2016-08-24 15:16:43 -0400

    fix

--- convolution/conv_mnist.py
+++ convolution/conv_mnist.py
@@ -27,11 +27,11 @@ h2_conv = tf.nn.relu(tf.nn.conv2d(h1_pool, w2, strides=[1,1,1,1], padding='SAME'
 h2_pool = tf.nn.max_pool(h2_conv, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
 h2_pool_flat = tf.reshape(h2_pool, shape=[-1, 7*7*64])
 h1_fc = tf.nn.relu(tf.matmul(h2_pool_flat, w3) + b3)
-h1_dropout = tf.nn.dropout(h1_fc, keep_prob=keep_prob)
-output = tf.nn.softmax(tf.matmul(h1_dropout, w4) + b4)
+# h1_dropout = tf.nn.dropout(h1_fc, keep_prob=keep_prob)
+output = tf.nn.softmax(tf.matmul(h1_fc, w4) + b4)
 
 # define loss function
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(target * tf.log(output), reduction_indices=[1]))
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(target * tf.log(tf.clip_by_value(output, 1e-10, 1.0)), reduction_indices=[1]))
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(output,1), tf.argmax(target,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -41,12 +41,14 @@ sess.run(tf.initialize_all_variables())
 for i in range(20000):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
+        weights = sess.run(w1)
+        print(weights)
         train_accuracy = sess.run(accuracy, feed_dict={
             x:batch[0], target: batch[1], keep_prob: 1.0})
         print("step %d, training accuracy %g"%(i, train_accuracy))
-    sess.run(train_step, feed_dict={x: batch[0], target: batch[1], keep_prob: 0.5})
+    sess.run(train_step, feed_dict={x: batch[0], target: batch[1], keep_prob: 1.0})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
+print("test accuracy %g"%sess.run(accuracy, feed_dict={
     x: mnist.test.images, target: mnist.test.labels, keep_prob: 1.0}))
 
 
